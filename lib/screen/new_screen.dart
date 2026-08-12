f2e2246afdbe449b9762d5475de45a0aab28b560
Naseem Khan <naseem236991@gmail.com>
2026-08-12 17:46:22 +0500

    new

--- lib/screen/new_screen.dart
+++ lib/screen/new_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class NewScreen extends StatelessWidget {
   const NewScreen({super.key});
-
+// this is the new screen
   @override
   Widget build(BuildContext context) {
     return Scaffold();
